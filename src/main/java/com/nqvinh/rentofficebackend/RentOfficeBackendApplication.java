fa63f11fe9e27de37457066a2687a3b3506bcdec
Nqvinh7603 <nqvinh7603@gmail.com>
2024-12-30 11:00:32 +0700

    first commit

---
+++ src/main/java/com/nqvinh/rentofficebackend/RentOfficeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nqvinh.rentofficebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentOfficeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentOfficeBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nqvinh/rentofficebackend/RentOfficeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nqvinh.rentofficebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentOfficeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
